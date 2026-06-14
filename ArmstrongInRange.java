96f0a70fe58ac0cc2812e9ce773a0301de110110
Avinash Mundolli <avinashmundolli@gmail.com>
2026-06-14 10:55:51 +0530

    ok

--- ArmstrongInRange.java
+++ ArmstrongInRange.java
@@ -1,3 +1,19 @@
 public class ArmstrongInRange {
-    
+    public static void main(String[] args) {
+        int s=100,e=1000;
+        
+        for(int i=s;i<=e;i++){
+            int sum=0;
+            String str=Integer.toString(i);
+            int n=str.length();
+            int j=i;
+            while(j!=0){
+            sum=sum+(int)Math.pow(j%10, n);
+            j/=10;
+            }
+            if(i==sum){
+                System.out.println(i+" ");
+            }
+        }
+    }
 }
