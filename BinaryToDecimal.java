a76eab9d21ea170d537dcd61e7ec9ab721cc1657
Avinash Mundolli <avinashmundolli@gmail.com>
2026-06-14 18:30:50 +0530

    ok

--- BinaryToDecimal.java
+++ BinaryToDecimal.java
@@ -1,3 +1,12 @@
 public class BinaryToDecimal {
-    
+    public static void main(String[] args) {
+        int Binary=1011;
+        int power=0,sum=0;
+        while(Binary!=0){
+            sum+=Binary%10*Math.pow(2, power);
+            power++;
+            Binary/=10;
+        }
+        System.out.println(sum);
+    }
 }
