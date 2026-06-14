182b43ba27c5a085d00b3e03c6ef49f26c55f553
Avinash Mundolli <avinashmundolli@gmail.com>
2026-06-14 11:49:41 +0530

    ok

--- Factorial.java
+++ Factorial.java
@@ -1,3 +1,13 @@
+
+import java.math.BigInteger;
+
 public class Factorial {
-    
+    public static void main(String[] args) {
+        int n=100;
+        BigInteger fact=BigInteger.ONE;
+        for(int i=2;i<n;i++){
+            fact=fact.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(fact);
+    }
 }
