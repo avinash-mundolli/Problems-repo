e1d0edb17911f477333c8781e0f981bb77481258
Avinash Mundolli <avinashmundolli@gmail.com>
2026-06-16 10:59:55 +0530

    ok

---
+++ SumOfArray.java
@@ -0,0 +1,10 @@
+public class SumOfArray {
+    public static void main(String[] args) {
+        int sum=0;
+        int[] arr = {5, 2, 9, 1, 7};
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+    }
+}
