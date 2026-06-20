680eb9a6b6f14f2ddc74b74d02a783b3b480280f
Avinash Mundolli <avinashmundolli@gmail.com>
2026-06-20 14:23:20 +0530

    ok

--- RepeatingElements.java
+++ RepeatingElements.java
@@ -1,10 +1,19 @@
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class RepeatingElements {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 2, 1, 4, 5, 3};
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==arr[i+1]){
-                System.out.println();
+        Set<Integer> seen=new HashSet<>();
+        Set<Integer> repeat=new HashSet<>();
+        for(int x:arr){
+            if(seen.contains(x)){
+                repeat.add(x);
+            }else{
+                seen.add(x);
             }
         }
+        System.out.println(repeat);
     }
 }
