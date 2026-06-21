1a3a9e5dd8a574fda444f4e2b3d707c7a828f68b
Avinash Mundolli <avinashmundolli@gmail.com>
2026-06-21 17:41:51 +0530

    ok

--- NonRepeatingElements.java
+++ NonRepeatingElements.java
@@ -1,8 +1,6 @@
 
 import java.util.HashSet;
 import java.util.Set;
-
-
 public class NonRepeatingElements {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 2, 1, 4, 5, 3};
--- SortByFrequency.java
+++ SortByFrequency.java
@@ -1,23 +1,19 @@
+
 import java.util.*;
 
 public class SortByFrequency {
     public static void main(String[] args) {
 
         int[] arr = {4, 5, 6, 5, 4, 3};
-
         HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            if(map.containsKey(arr[i])){
-                map.put(arr[i], map.get(arr[i])+1);
-            }else{
-                map.put(arr[i], 1);
-            }
+        for(int x:arr){
+            map.put(x, map.getOrDefault(x, 0)+1);
         }
         Integer[] temp=new Integer[arr.length];
         for(int i=0;i<arr.length;i++){
             temp[i]=arr[i];
         }
-        Arrays.sort(temp, new Comparator<Integer>(){
+        Arrays.sort(temp, new Comparator<Integer>() {
             @Override
             public int compare(Integer a,Integer b){
                 if(map.get(a)!=map.get(b)){
