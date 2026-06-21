3d53ac93df12469a242ee663fac94d0284781daf
Avinash Mundolli <avinashmundolli@gmail.com>
2026-06-21 17:22:36 +0530

    ok

--- FrequencyInArray.java
+++ FrequencyInArray.java
@@ -1,13 +1,13 @@
+
+import java.util.HashMap;
+
 public class FrequencyInArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 2, 1, 3, 2, 1, 1};
-        int target=2;
-        int count=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==target){
-                count++;
-            }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int x:arr){
+            map.put(x, map.getOrDefault(x, 0)+1);
         }
-        System.out.println(count);
+        System.out.println(map);
     }
 }
