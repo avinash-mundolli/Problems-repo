23f37cff26c097547eaf981bf3b515716a754007
Avinash Mundolli <avinashmundolli@gmail.com>
2026-06-24 22:29:44 +0530

    ok

---
+++ StringLength.java
@@ -0,0 +1,10 @@
+public class StringLength {
+    public static void main(String[] args) {
+        int length=0;
+        String str="avinashmundolli";
+        for(char c:str.toCharArray()){
+            length++;
+        }
+        System.out.println(length);
+    }
+}
