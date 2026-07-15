edc233a21e8bcf8ae8e03a195ea7c71ba0ad4436
Avinash Mundolli <avinashmundolli@gmail.com>
2026-07-15 11:17:29 +0530

    ok

--- Demo.java
+++ Demo.java
@@ -1,16 +1,29 @@
 public class Demo {
     public static void main(String[] args) {
 
-        int[] arr = {2, 5, 1, 8, 2, 9, 1};
-        int k = 3;
+        int[] arr = {2, 5, 4, 8, 2, 9, 1};
+        int k = 4;
         int count = 0;
-        for(int i=0;i<arr.length-k+1;i++){
-           if(i%2==0){
-            count++;
-           }
-
+        int count2=0;
+            for( int i=0;i<k;i++){
+                if(i%2==0){
+                    count++;
+                }
+                if(count>k/2){
+                    count2++;
+                }
         }
-
-        System.out.println(count);
+    
+        for(int i=k;i<arr.length;i++){
+            if(arr[i-k]%2==0){
+                count--;
+            }else if(arr[i]%2==0){
+                count++;
+            }
+            if(count>k/2){
+                count2++;
+            }
+        }
+        System.out.println(count2);
     }
 }
